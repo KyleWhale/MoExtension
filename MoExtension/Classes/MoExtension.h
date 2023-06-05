7eb9ce0d04b3e54bba17aec4d5304d2a55ece06e
kylej <kylej@bluewhaletech.pro>
2023-06-05 18:47:12 +0800

    0.0.3

--- MoExtension/Classes/MoExtension.h
+++ MoExtension/Classes/MoExtension.h
@@ -1,12 +1,16 @@
 
-#ifndef MoExtension_h
-#define MoExtension_h
-
+#if __has_include(<MoExtension/MoExtension.h>)
 #import <MoExtension/NSDate+Extension.h>
 #import <MoExtension/UIView+Extension.h>
 #import <MoExtension/UIColor+Extension.h>
 #import <MoExtension/UIDevice+Extension.h>
 #import <MoExtension/UIButton+Extension.h>
 #import <MoExtension/NSString+Extension.h>
-
-#endif /* MoExtension_h */
+#else
+#import "NSDate+Extension.h"
+#import "UIView+Extension.h"
+#import "UIColor+Extension.h"
+#import "UIDevice+Extension.h"
+#import "UIButton+Extension.h"
+#import "NSString+Extension.h"
+#endif
